f5f3d9cffc5f7b79c00a1ff82ed3e96614ba257c
Venugopal K <venu.shastri@gmail.com>
2025-10-15 10:09:29 +0530

    Recover from Build Error

--- string_calculator.cpp
+++ string_calculator.cpp
@@ -2,4 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include "string_calculator.h"
 
+AddResult add(const char* input) {
+    fprintf(stderr, "Error: Fnction Not Implemented Yet")
+    exit(EXIT_FAILURE);
+}
